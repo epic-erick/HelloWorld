83a76a72bdd0556ca6270a081a980e5cc8b3e8f8
Erick Tepale <e.tepale@gmail.com>
2017-12-29 01:31:37 -0500

    Initial commit, Version Control Test

---
+++ src/App.java
@@ -0,0 +1,11 @@
+/*
+* Created By Epic_ on 12/29/2017
+* */
+
+public class App {
+    //psvm <-- Shortcut for public static void main(String[] args){}
+    public static void main(String[] args) {
+        //sout <-- Shortcut for System.out.println();
+        System.out.println("Hello World");
+    }
+}
